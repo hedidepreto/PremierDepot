65fbe50cd28bc1511a4cea8c4f9963ebd6a2902e
hedidepreto <128788040+hedidepreto@users.noreply.github.com>
2023-04-27 23:44:40 +0200

    Update Operations.h

--- Operations.h
+++ Operations.h
@@ -1,4 +1,12 @@
-Hello World
+
 int addition(int a, int b);
 
 int soustraction(int a, int b);
+
+int factoriel(int nb) {
+    int resultat = 1;
+    for (int i = 1; i <= nb; i++) {
+        resultat *= i;
+    }
+    return resultat;
+}
