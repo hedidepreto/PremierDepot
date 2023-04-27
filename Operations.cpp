b994c670a672b90cbdde774eecc0dc8d61dab887
hedidepreto <128788040+hedidepreto@users.noreply.github.com>
2023-04-27 23:37:03 +0200

    Update Operations.cpp

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,19 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+
+int multiplication(int nombre1, int nombre2) {
+    int resultat = nombre1 * nombre2;
+    return resultat;
+}
+
+float division(float nombre1, float nombre2) {
+    if (nombre2 == 0) {
+        //  division par zéro
+        cout << "la divison par zero est interdite" << endl;
+        return 0;
+    } else {
+        float resultat = nombre1 / nombre2;
+        return resultat;
+    }
+}
